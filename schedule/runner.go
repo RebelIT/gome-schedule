7b2d5bf26ae6361fb724bfeb72a596c221f92d60
RebelIT <31414479+RebelIT@users.noreply.github.com>
2020-09-01 23:14:53 -0500

    runners

---
+++ schedule/runner.go
@@ -0,0 +1,24 @@
+package schedule
+
+import (
+	"log"
+	"time"
+)
+
+//ToDo: left off here
+func ProcessSchedules(){
+	log.Printf("INFO: Starting schedule runners")
+
+	for{
+		dt := time.Now()
+
+	}
+}
+
+func processToggleSchedules(){
+	getAllToggleSchedules()
+}
+
+func processStateSchedules(){
+	getAllStateSchedules()
+}
